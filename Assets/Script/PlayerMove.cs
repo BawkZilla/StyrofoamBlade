e47f34c9cadf2dd70b5e5cad6ce834d7e026c6dd
Yun <ghini7170@gmail.com>
2025-07-18 22:51:31 +0900

    Feat : Tutorial 6 - Movement2 Implemented

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,6 +4,12 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerMove : MonoBehaviour
 {
     float _moveSpeed = 3.5f;
+    [SerializeField] float _downForce = 5f;
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask _groundLayer;
+    bool _isGrounded = false;
+
     float _animSpeed = 0f;
     bool _isRunning = false;
     Rigidbody2D _rb;
@@ -17,9 +23,13 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        GroundCheck();
+        if (Input.GetButtonDown("Jump") && _isGrounded) Jump();
+
         float _xHor = Input.GetAxis("Horizontal");
         _isRunning = Input.GetButton("Sprint");
-        _rb.linearVelocity = new Vector2(_xHor * _moveSpeed, 0f);
+        _rb.linearVelocity = new Vector2(_xHor * _moveSpeed, _rb.linearVelocity.y);
+        _rb.linearVelocityX = _xHor * _moveSpeed * (_isRunning ? 2 : 1);
 
         if (_xHor > 0)
         {
@@ -39,6 +49,32 @@ public class PlayerMove : MonoBehaviour
             _animSpeed = 0f;
         }
         _anim.SetFloat("_speed", _animSpeed);
+    }
 
+    void Jump() => _rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+    private void FixedUpdate()
+    {
+        AddDownForce();
+    }
+
+    void GroundCheck()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+        _anim.SetBool("_isAir", !_isGrounded);
+    }
+
+    void AddDownForce()
+    {
+        _rb.AddForce(Vector2.down * _downForce);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (_groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+        }
     }
 }
