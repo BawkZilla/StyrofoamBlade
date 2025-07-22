99025c52ab6b5e4d60cb2e26800c5302a3297fdf
Yun <ghini7170@gmail.com>
2025-07-22 16:59:13 +0900

    Feat : Roll

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMove : MonoBehaviour
 {
+    float _xHor;
     float _moveSpeed = 3.5f;
     float _animSpeed = 0f;
     [SerializeField] float _downForce = 10f;
@@ -17,6 +19,15 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] LayerMask _groundLayer;
     bool _isGrounded = false;
 
+    [Header("Roll")]
+    [SerializeField] float _rollPower;
+    [SerializeField] float _rollDuration;
+    [SerializeField] float _rollCooldown;
+
+    int _playerDir = 1;
+
+    bool _isRolling = false;
+    bool _canRoll = true;
 
     void Start()
     {
@@ -27,33 +38,9 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         GroundCheck();
-
-        float _xHor = Input.GetAxis("Horizontal");
-        _isRunning = Input.GetButton("Sprint");
-
-        if (Input.GetButtonDown("Jump")) Jump();
-
-        _rb.linearVelocity = new Vector2(_xHor * _moveSpeed * (_isRunning ? 2 : 1), _rb.linearVelocity.y);
-
-        if (_xHor > 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (_xHor < 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-        }
-
-        if (_xHor != 0)
-        {
-            _animSpeed = _isRunning ? 1f : 0.5f;
-        }
-        else
-        {
-            _animSpeed = 0f;
-        }
-        _anim.SetFloat("_speed", _animSpeed); 
-
+        Move();
+        Jump();
+        Roll();
     }
 
     private void FixedUpdate()
@@ -76,9 +63,66 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    void Roll()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl) && _canRoll) StartCoroutine(RollCoroutine());
+    }
+
+    IEnumerator RollCoroutine()
+    {
+        _anim.Play("Roll");
+
+        _isRolling = true;
+        _canRoll = false;
+
+        _rb.linearVelocityX = _playerDir * _rollPower;
+
+        yield return new WaitForSeconds(_rollDuration);
+
+        _rb.linearVelocity = Vector2.zero;
+        _isRolling = false;
+
+        yield return new WaitForSeconds(_rollCooldown);
+
+        _canRoll = true;
+    }
+
+    void Move()
+    {
+        if (_isRolling) return;
+
+        _xHor = Input.GetAxis("Horizontal");
+        _isRunning = Input.GetButton("Sprint");
+        _rb.linearVelocity = new Vector2(_xHor * _moveSpeed * (_isRunning ? 2 : 1), _rb.linearVelocity.y);
+
+        if (_xHor > 0)
+        {
+            _playerDir = 1;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (_xHor < 0)
+        {
+            _playerDir = -1;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (_xHor != 0)
+        {
+            _animSpeed = _isRunning ? 1f : 0.5f;
+        }
+        else
+        {
+            _animSpeed = 0f;
+        }
+        _anim.SetFloat("_speed", _animSpeed);
+    }
+
     void Jump()
     {
-        _rb.AddForce(Vector2.up * _jumpForce , ForceMode2D.Impulse);
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
     }
     void AddDownForce()
     {
