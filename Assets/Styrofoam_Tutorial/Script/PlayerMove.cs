51c56e201a8a607208011e34c03d1259deda2cf2
Yun <ghini7170@gmail.com>
2025-07-29 11:14:08 +0900

    Refactor : 코드 리팩토링 및 패키지 Export

---
+++ Assets/Styrofoam_Tutorial/Script/PlayerMove.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class PlayerMove : MonoBehaviour
+{
+    float _xHor;
+    float _animSpeed = 0f;
+    [SerializeField] float _moveSpeed = 3.5f;
+    [SerializeField] float _downForce = 10f;
+    [SerializeField] float _jumpForce = 5f;
+    bool _isRunning = false;
+    Rigidbody2D _rb;
+    Animator _anim;
+
+    [Header("GroundCheck")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundCheckRadius = .2f;
+    [SerializeField] LayerMask _groundLayer;
+    bool _isGrounded = false;
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        Move();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        AddDownForce();
+    }
+
+    void GroundCheck()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+        _anim.SetBool("_isAir", !_isGrounded);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(_groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+        }
+    }
+
+    void Move()
+    {
+        _xHor = Input.GetAxis("Horizontal");
+        _isRunning = Input.GetButton("Sprint");
+        _rb.linearVelocity = new Vector2(_xHor * _moveSpeed * (_isRunning ? 2 : 1), _rb.linearVelocity.y);
+
+        if (_xHor > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (_xHor < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (_xHor != 0)
+        {
+            _animSpeed = _isRunning ? 1f : 0.5f;
+        }
+        else
+        {
+            _animSpeed = 0f;
+        }
+        _anim.SetFloat("_speed", _animSpeed);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    void AddDownForce()
+    {
+        _rb.AddForce(Vector2.down * _downForce);
+    }
+}
